6b30839aacc89ba3ec397e9d277784c4ff61d6f9
noiseymur <65159986+noiseymur@users.noreply.github.com>
2020-12-19 02:23:50 +0400

    Update sm-carousel.js

--- scripts/sm-carousel.js
+++ scripts/sm-carousel.js
@@ -28,7 +28,7 @@ carousels.forEach(carousel=>{
     
     scroller.addEventListener('scroll',resizeOnScroll);
     if(scroller.children.length!==0){
-        scroller.scrollTo({left: slideWidth*1.5});
+        scroller.scrollBy({left: slideWidth*1.5});
         console.log(scroller.children);
     }
     else {
