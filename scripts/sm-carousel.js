09ae728e8ea7835d184b718f597262ae0d078042
noiseymur <65159986+noiseymur@users.noreply.github.com>
2020-12-19 16:10:55 +0400

    Update sm-carousel.js

--- scripts/sm-carousel.js
+++ scripts/sm-carousel.js
@@ -1,6 +1,92 @@
-import dataList from './data.js';
+// import dataList from './data.js';
 
-document.addEventListener('DOMContentLoaded',()=>{
+const dataList = [
+    {
+        name: 'The Art Piece 1',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic1.jpg'
+    },
+    {
+        name: 'The Art Piece 2',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic2.jpg'
+    },
+    {
+        name: 'The Art Piece 3',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic3.jpg'
+    },
+    {
+        name: 'The Art Piece 4',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic4.jpg'
+    },
+    {
+        name: 'The Art Piece 5',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic5.jpg'
+    },
+    {
+        name: 'The Art Piece 6',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic6.jpg'
+    },
+    {
+        name: 'The Art Piece 7',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic7.jpg'
+    },
+    {
+        name: 'The Art Piece 8',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic8.jpg'
+    },
+    {
+        name: 'The Art Piece 9',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic9.jpg'
+    },
+    {
+        name: 'The Art Piece 10',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic10.jpg'
+    },
+    {
+        name: 'The Art Piece 11',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic11.jpg'
+    },
+    {
+        name: 'The Art Piece 12',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic12.jpg'
+    },
+    {
+        name: 'The Art Piece 13',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic13.jpg'
+    },
+    {
+        name: 'The Art Piece 14',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic14.jpg'
+    },
+    {
+        name: 'The Art Piece 15',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic15.jpg'
+    },
+    {
+        name: 'The Art Piece 16',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic16.jpg'
+    },
+    {
+        name: 'The Art Piece 17',
+        price: '$49.90',
+        path: 'https://noiseymur.github.io/canvas-store/artpaths/artpic17.jpg'
+    }
+];
 
 let carousels = document.querySelectorAll('.sm-carousel');
 
@@ -29,9 +115,6 @@ carousels.forEach(carousel=>{
     initializeSlides();
     
     scroller.addEventListener('scroll',resizeOnScroll);
-    setTimeout(()=>{
-        scroller.scrollTo({left: slideWidth*1.5});
-    },0);
     scroller.scrollTo({left: slideWidth*1.5});
     scroller.addEventListener('click',scrollOnTap);
     
@@ -190,6 +273,5 @@ function populateCarousels (item,index) {
         scroller.appendChild(slideItem);
     }
 }
-    
-});
+
 
